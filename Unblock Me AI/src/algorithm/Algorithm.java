b0098f84f80f46da4657b38cade71837d1fa5489
Unknown <dinismoreira1997@gmail.com>
2019-03-29 14:41:30 +0000

    Update

--- Unblock Me AI/src/algorithm/Algorithm.java
+++ Unblock Me AI/src/algorithm/Algorithm.java
@@ -1,12 +1,16 @@
 package algorithm;
 
+import elements.*;
+import java.util.ArrayList;
+import java.util.EmptyStackException;
 import elements.Board;
 import java.util.Stack;
-import java.util.ArrayList;
 
 import elements.Vertex;
 
 public abstract class Algorithm {
+    
+    protected int index = 0;
     protected Board initialBoard;
     protected Stack<Vertex> stack;
     protected ArrayList<Vertex> allVertexes = new ArrayList<Vertex>();
@@ -14,6 +18,82 @@ public abstract class Algorithm {
     Algorithm(Board board) {
         this.initialBoard = board;
         this.stack = new Stack<Vertex>();
+    }
+
+
+    public void generateVertexChildren(Vertex vertex){
+        ArrayList<ArrayList<Move>> allMoves = vertex.getBoard().getAllMoves();
+
+        /*System.out.println();
+        System.out.println("//////////////////////////////////");
+        System.out.println("Children of:");
+        vertex.getBoard().printBoard();
+        System.out.println("-----------------------------------");*/
+
+        /*System.out.println("+++++++++ PARENT PAST BOARDS +++++++++");
+        vertex.displayPastBoards();
+        System.out.println("++++++++++++++++++++++++++++++++++++++");*/
+
+        for(int i=0; i<allMoves.size();i++){
+            for(int j=0; j<allMoves.get(i).size();j++){
+                //Add current board to the new pastBoards List for children
+                ArrayList<Board> newPastBoards = new ArrayList<Board>();
+                for(int k = 0; k < vertex.getPastBoards().size(); k++){
+                    newPastBoards.add(vertex.getPastBoards().get(k));
+                }
+                newPastBoards.add(allMoves.get(i).get(j).getNewBoard());
+
+
+                
+                if(checkRepeatedVertex(allMoves.get(i).get(j).getNewBoard())){
+                    //Create new Vertex
+                    Vertex newChild = new Vertex(allMoves.get(i).get(j).getNewBoard(), vertex.getDepth()+1, newPastBoards);
+
+                    //ADD CHILD TO STACK
+                    this.stack.push(newChild);
+                    this.allVertexes.add(newChild);
+
+                    //ADD CHILD TO vertex.neighbours
+                    vertex.getNeighbours().add(newChild);
+
+                    
+
+                    //SEE CHILD
+                    /*newChild.getBoard().printBoard();
+                    System.out.println("NEW CHILD");
+                    System.out.println("******** PAST BOARDS *********");
+                    newChild.displayPastBoards();
+                    System.out.println("******************************");*/
+                }
+            }
+        }
+
+        //System.out.println("//////////////////////////////////");
+
+        return;
+    }
+
+
+    public Boolean checkRepeatedVertex(Board board){
+        
+        for(int i = 0; i < this.allVertexes.size(); i++){
+            if(board.equals(this.allVertexes.get(i).getBoard())){
+                //System.out.println("REPEATED BOARD!!!");
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+    public void displayVertex(Vertex vertex){
+        System.out.println();
+        System.out.println("V. index: " + index);
+        vertex.getBoard().printBoard();
+        System.out.println("Depth: " + vertex.getDepth());
+        System.out.println("Stack Size: " + this.stack.size());
+        System.out.println("Number of V.: " + this.allVertexes.size());           
+        index++;
     }
 
 }
--- Unblock Me AI/src/algorithm/DFS.java
+++ Unblock Me AI/src/algorithm/DFS.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class DFS extends Algorithm {
-    public int index = 0;
 
     public DFS(Board board) {
         super(board);
@@ -13,15 +12,11 @@ public class DFS extends Algorithm {
 
     public Boolean solve(int maxDepth){
 
-
-
         ArrayList<Board> pastBoards = new ArrayList<Board>();
         pastBoards.add(this.initialBoard);
 
         Vertex root = new Vertex(this.initialBoard, 0, pastBoards);
 
-
-        
         Vertex solution = exploreRoot(root, maxDepth);
 
         if(solution == null){
@@ -45,7 +40,6 @@ public class DFS extends Algorithm {
         */
     }
 
-    //UNTESTED & TO DO
     public Vertex exploreRoot(Vertex root, int maxDepth){
         Vertex solution = null;
 
@@ -64,8 +58,7 @@ public class DFS extends Algorithm {
         return solution;
     }
 
-    
-    //UNTESTED & TO DO
+
     public Vertex exploreGraph(int maxDepth){
         Vertex vertex;
         if(this.stack.size() <= 0){
@@ -84,7 +77,6 @@ public class DFS extends Algorithm {
         //PRINT VERTEX BOARD
         //displayVertex(vertex);
         
-
         if(vertex.getBoard().checkVictory()){
             return vertex;
         }
@@ -96,87 +88,7 @@ public class DFS extends Algorithm {
         return exploreGraph(maxDepth);
     }
 
-    //UNTESTED & TO DO
-    public void generateVertexChildren(Vertex vertex){
-        ArrayList<ArrayList<Move>> allMoves = vertex.getBoard().getAllMoves();
 
-        /*System.out.println();
-        System.out.println("//////////////////////////////////");
-        System.out.println("Children of:");
-        vertex.getBoard().printBoard();
-        System.out.println("-----------------------------------");*/
-
-        
-        /*System.out.println("+++++++++ PARENT PAST BOARDS +++++++++");
-        vertex.displayPastBoards();
-        System.out.println("++++++++++++++++++++++++++++++++++++++");*/
-
-        for(int i=0; i<allMoves.size();i++){
-            for(int j=0; j<allMoves.get(i).size();j++){
-                //Add current board to the new pastBoards List for children
-                ArrayList<Board> newPastBoards = new ArrayList<Board>();
-                for(int k = 0; k < vertex.getPastBoards().size(); k++){
-                    newPastBoards.add(vertex.getPastBoards().get(k));
-                }
-                newPastBoards.add(allMoves.get(i).get(j).getNewBoard());
-
-
-                
-                if(checkRepeatedVertex(allMoves.get(i).get(j).getNewBoard())){
-                    //Create new Vertex
-                    Vertex newChild = new Vertex(allMoves.get(i).get(j).getNewBoard(), vertex.getDepth()+1, newPastBoards);
-
-                    //ADD CHILD TO STACK
-                    this.stack.push(newChild);
-                    this.allVertexes.add(newChild);
-
-                    //ADD CHILD TO vertex.neighbours
-                    vertex.getNeighbours().add(newChild);
-
-                    
-
-                    //SEE CHILD
-                    /*newChild.getBoard().printBoard();
-                    System.out.println("NEW CHILD");
-                    System.out.println("******** PAST BOARDS *********");
-                    newChild.displayPastBoards();
-                    System.out.println("******************************");*/
-                }
-
-                
-                //newPastBoards.remove(newPastBoards.size()-1);
-            }
-        }
-
-        //System.out.println("//////////////////////////////////");
-
-
-        return;
-    }
-
-    //UNTESTED
-    public Boolean checkRepeatedVertex(Board board){
-        
-        for(int i = 0; i < this.allVertexes.size(); i++){
-            if(board.equals(this.allVertexes.get(i).getBoard())){
-                //System.out.println("REPEATED BOARD!!!");
-                return false;
-            }
-        }
-
-        return true;
-    }
     
-
-    public void displayVertex(Vertex vertex){
-        System.out.println();
-        System.out.println("V. index: " + index);
-        vertex.getBoard().printBoard();
-        System.out.println("Depth: " + vertex.getDepth());
-        System.out.println("Stack Size: " + this.stack.size());
-        System.out.println("Number of V.: " + this.allVertexes.size());           
-        index++;
-    }
-
 
 }
